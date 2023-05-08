409a9c3f4d349b05cecef773ed0dcb483115c0ab
6161990 <6161990src@gmail.com>
2023-05-09 06:17:41 +0900

    setUp kafkaConfig

---
+++ demo/src/main/java/com/yoon/demo/config/KafkaConfig.java
@@ -0,0 +1,35 @@
+package com.yoon.demo.config;
+
+import lombok.Getter;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+    /** connection 정보를 갖고 있는 template */
+    @Bean
+    public KafkaTemplate<String, Object> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+    /** serialize 할 설정을 담은 connection 정보*/
+    @Bean
+    public ProducerFactory<String, Object> producerFactory(){
+        Map<String, Object> config = new HashMap<>();
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "3.42.324.235"); /** public Ipv4 주소*/
+        config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return new DefaultKafkaProducerFactory<>(config);
+    }
+}
